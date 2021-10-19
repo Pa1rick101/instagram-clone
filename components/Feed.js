0dc669e1129ae4f1a566f7b2916c94f69fc72ca7
Patrick <patrick.suehrer@thedac.info>
2021-10-19 08:04:27 +0200

    addeed feeeed

---
+++ components/Feed.js
@@ -0,0 +1,17 @@
+function Feed() {
+  return (
+    <main>
+      <section>
+       {/* Stories */}
+       {/* Posts */}
+      </section>
+     
+      <section>
+        {/* Mini profile */}
+        {/* Suggestions */}
+      </section>        
+    </main>
+  )
+}
+
+export default Feed
